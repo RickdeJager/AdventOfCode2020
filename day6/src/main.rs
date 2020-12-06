b6e9ed3d9acada32196044b0eb6a118f1c245871
Rick de Jager <rickdejager99@gmail.com>
2020-12-06 12:11:10 +0100

    bad solve

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -18,8 +18,26 @@ fn part1(input: &Vec<String>) -> usize {
          .sum()
 }
 
+fn part2(input: &Vec<String>) -> usize {
+    let mut c = 0;
+    for group in input {
+        let mut qs: Vec<char>  = group.split("\n").next().unwrap().chars().collect();
+        for p in group.split("\n") {
+            qs = qs
+                .iter()
+                .filter(|q| p.contains(&q.to_string()))
+                .map(|q| *q)
+                .collect();
+        }
+        c += qs.len();
+    }
+    c
+}
+
 fn main() {
     let input = read();
     println!("===== Part 1 ====");
     println!("num is {}", part1(&input));
+    println!("===== Part 2 ====");
+    println!("num is {}", part2(&input));
 }
