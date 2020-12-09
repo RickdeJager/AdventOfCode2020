9bcb604786b3627a327cd6bf84d9f956ed8cfdbd
Rick de Jager <rickdejager99@gmail.com>
2020-12-09 11:00:53 +0100

    Day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,13 +7,13 @@ fn read() -> Vec<usize> {
 }
 
 fn part1(input: &Vec<usize>) -> usize {
-    const Window: usize = 25;
+    const WINDOW: usize = 25;
     for (i, num) in input.iter().enumerate() {
-        if i < Window {
+        if i < WINDOW {
             continue;
         }
         let mut flag = false;
-        for j in i-Window..i {
+        for j in i-WINDOW..i {
             for k in j..i {
                 if input[j] + input[k] == *num {
                     flag = true;
@@ -27,9 +27,40 @@ fn part1(input: &Vec<usize>) -> usize {
     unreachable!();
 }
 
+fn part2(input: &Vec<usize>, goal: usize) -> usize {
+    let mut first = 0;
+    let mut last  = 0;
+    let mut acc   = input[0];
+    while last < input.len() {
+        // We've overshot the goal :O.
+        // Since the set is continuous, we can only kick out earlier elements
+        while acc > goal {
+            acc   -= input[first];
+            first += 1;
+        }
+        // First could be last+1, in case we just kicked out an element > goal
+        if last < first {
+            last = first
+        }
+        // Now we can start adding elements
+        while acc < goal {
+            last += 1;
+            acc  += input[last];
+        }
+        // If we've summed to the exact goal, return here
+        if acc == goal {
+            return input[first..last+1].iter().min().unwrap() +
+                   input[first..last+1].iter().max().unwrap();
+        }
+    }
+    unreachable!();
+}
+
 fn main() {
     let input = read();
     println!("===== Part 1 ====");
-    println!("The first number is {}", part1(&input));
+    let p1 = part1(&input);
+    println!("The first number is {}", p1);
     println!("===== Part 2 ====");
+    println!("The second number is {}", part2(&input, p1));
 }
